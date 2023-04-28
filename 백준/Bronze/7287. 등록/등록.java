9a833dedf3c6091b73f9a9fcc9a69d095b7d6a5e
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-04-28 11:04:35 +0900

    [Bronze V] Title: 등록, Time: 124 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("5");
+		bw.newLine();
+
+		bw.write("dayoungrenakang");
+		bw.newLine();
+
+		bw.flush();
+		bw.close();
+
+	}
+}
