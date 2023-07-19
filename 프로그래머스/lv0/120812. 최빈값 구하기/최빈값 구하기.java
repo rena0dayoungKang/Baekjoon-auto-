0ba1eaa89b2c664db5e3db88fb413e543f03e1ca
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-07-19 17:13:58 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.11 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        int maxCount = 0;
+        for (int number : array) {
+            int count = map.getOrDefault(number, 0) + 1;
+            if (count > maxCount) {
+                maxCount = count;
+                answer = number;
+            } else if (count == maxCount) {
+                answer = -1;
+            }
+            map.put(number, count);
+        }       
+        return answer;
+    }
+}
