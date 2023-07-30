c15f65cf6a915b9336562f8a91969e322157205c
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-07-31 01:24:50 +0900

    [unrated] Title: 날짜 비교하기, Time: 1.20 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -2,16 +2,16 @@ class Solution {
     public int solution(int[] date1, int[] date2) {
         int answer = 0;
         
-        if (date1[0] < date2[0]) {
+          String date1Str = String.format("%04d%02d%02d", 
+                                          date1[0], date1[1], date1[2]);
+        String date2Str = String.format("%04d%02d%02d", 
+                                        date2[0], date2[1], date2[2]);
+        
+        int a = Integer.parseInt(date1Str);
+        int b = Integer.parseInt(date2Str);
+        
+        if (a < b) {
             answer = 1;
-        } else if (date1[0] == date2[0]) {
-            if (date1[1] < date2[1]) {
-                answer = 1;
-            } else if (date1[1] == date2[1]) {
-                if (date1[2] < date2[2]) {
-                    answer = 1;
-                } 
-            }
         }
         
         return answer;
