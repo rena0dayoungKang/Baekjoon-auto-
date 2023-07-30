fcc523857cd479e53f9354ac9ccf0a531fa91d42
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-07-30 12:45:06 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 58.95 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -1,37 +1,35 @@
 import java.util.*;
 class Solution {
     public String[] solution(String myString) {
-        
+      
         List<String> list = new ArrayList<>();
-        
-        String[] str = myString.split("");        
+        String[] strArray = myString.split("");
         String tmp = "";
-        for (int i = 0; i < str.length; i++) {
-            if (!str[i].equals("x")) {
-                tmp += str[i];
+        for (int i = 0; i < strArray.length; i++) {
+            if (!strArray[i].equals("x")) {
+                tmp += strArray[i];    
             } else {
-                if(tmp.equals("")){
-                    continue;
-                } else {
+                if (!tmp.equals("")) {
                     list.add(tmp);
                     tmp = "";
+                } else {
+                    continue;
                 }
-            } 
-        }        
-        // 1. !x -- tmp에 추가
-        // 2. x  -- tmp가 비었다면 pass
-        //       -- tmp가 안비었다면 list에 추가, tmp를 공백으로 해준다,
+            }
+        }
         
         if (!tmp.equals("")) {
-            list.add(tmp);     
-        } 
-        // 3. 마지막에 끝났을 떄 tmp에 문자가 있으면 list에추가
-        //                    tmp가 비어있으면 pass
-               
+            list.add(tmp);
+        }
         
         Collections.sort(list);
         
-        String[] answer = list.toArray(new String[0]);
+        String[] answer = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        
         
         return answer;
     }
