bb04a28f0fb5c52da2afdd05804a0cb23eed17a4
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-10-27 13:00:08 +0900

    [Bronze V] Title: A/B, Time: 204 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+        
+        double C = A / B;
+        System.out.println(C);
+    }
+}
