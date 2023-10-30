4f06d9d3244a2834c15c5187569c95580b008d9a
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-10-30 23:28:19 +0900

    [Bronze III] Title: 곱셈, Time: 116 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -3,19 +3,23 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int A, B;
-        A = Integer.parseInt(br.readLine());
-        B = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(br.readLine());
+        String B = br.readLine();
         
         int b0, b1, b2;
-        b2 = B / 100;
-        b1 = (B % 100) / 10;
-        b0 = B % 10;
+        //b2 = B / 100;
+        //b1 = (B % 100) / 10;
+        //b0 = B % 10;
+        b2 = Character.getNumericValue(B.charAt(0));
+        b1 = Character.getNumericValue(B.charAt(1));
+        b0 = Character.getNumericValue(B.charAt(2));
+        
+        int b = Integer.parseInt(B);
         
         System.out.println(A*b0);
         System.out.println(A*b1);
         System.out.println(A*b2);
-        System.out.println(A*B);
+        System.out.println(A*b);
         
     }
 }
