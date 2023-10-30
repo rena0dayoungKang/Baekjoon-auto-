dbacfdfaa6736a9a0adbffaa4769cd1069ced122
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-10-30 22:37:28 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        
+        int A, B, C;
+        A = Integer.parseInt(inputs[0]);
+        B = Integer.parseInt(inputs[1]);
+        C = Integer.parseInt(inputs[2]);
+        
+        System.out.println((A+B)%C);
+        System.out.println(((A%C)+(B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);      
+    }
+}
