227312967985751806c4f00363d4ac25f18c0b74
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-11-01 23:38:44 +0900

    [Bronze III] Title: 알람 시계, Time: 148 ms, Memory: 16100 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -7,20 +7,11 @@ public class Main{
         int H = Integer.parseInt(inputs[0]);
         int M = Integer.parseInt(inputs[1]);
         
-        if (H > 0) {
-            if (M >= 45 && M <= 59) {
-                M -= 45;
-            } else {
-                H -= 1;
-                M = 60 + M - 45;
-            }
-        } else if (H == 0) {
-            if (M >= 45 && M <= 59) {
-                M -= 45;
-            } else {
-                H = 23;
-                M = 60 + M - 45;
-            }
+        if (M >= 45) {
+            M -= 45;
+        } else {
+            H = (H + 23) % 24;
+            M = 60 + M - 45;
         }
         
         System.out.println(H + " " + M);
