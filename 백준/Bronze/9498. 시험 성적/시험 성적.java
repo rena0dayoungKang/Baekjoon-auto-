e7341d259ba6016892640dcf73db988df0d8196f
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-11-01 09:40:40 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -4,16 +4,20 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int score = Integer.parseInt(br.readLine());
         
+        String grade; 
+        
         if (score >= 90){
-            System.out.println("A");
+            grade = "A";
         } else if (score >= 80) {
-            System.out.println("B");
+            grade = "B";
         } else if (score >= 70) {
-            System.out.println("C");
+            grade = "C";
         } else if (score >= 60) {
-            System.out.println("D");
+            grade = "D";
         } else {
-            System.out.println("F");
+            grade = "F";
         }
+        
+        System.out.println(grade);
     }
 }
