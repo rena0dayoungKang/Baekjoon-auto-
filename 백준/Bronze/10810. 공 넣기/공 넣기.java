3cbb041f1d12ff4c23f22d918499927ec4fe72dd
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-11-10 15:06:14 +0900

    [Bronze III] Title: 공 넣기, Time: 156 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] inputTypes = br.readLine().split(" ");
+        int N = Integer.parseInt(inputTypes[0]);
+        int M = Integer.parseInt(inputTypes[1]);
+        int[] baskets = new int[N];
+        
+        for (int i = 0; i < M; i++) {
+            String[] inputNums = br.readLine().split(" ");
+            int I = Integer.parseInt(inputNums[0]);
+            int J = Integer.parseInt(inputNums[1]);
+            int K = Integer.parseInt(inputNums[2]);
+            
+            for (int j = I-1; j < J; j++) {
+                baskets[j] = K;
+            }
+        }
+        
+        for (int i = 0; i < N; i++) {
+            System.out.print(baskets[i] + " ");
+        }
+    }
+}
