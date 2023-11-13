cb7c5afa7d2fec257731b0c38681f2a7e3d0c3c6
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-11-13 12:45:32 +0900

    [Bronze II] Title: 공 바꾸기, Time: 152 ms, Memory: 15968 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputNums = br.readLine().split(" ");
+        int N = Integer.parseInt(inputNums[0]);
+        int M = Integer.parseInt(inputNums[1]);
+        int[] baskets = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            baskets[i] = (i+1);
+        }
+        
+        
+        for (int k = 0; k < M; k++) {
+            String[] switchs = br.readLine().split(" ");
+            int i = Integer.parseInt(switchs[0]);
+            int j = Integer.parseInt(switchs[1]);
+            
+            int tmp = baskets[i-1];
+            baskets[i-1] = baskets[j-1];
+            baskets[j-1] = tmp;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            System.out.print(baskets[i] + " ");
+        }
+        
+    }
+}
