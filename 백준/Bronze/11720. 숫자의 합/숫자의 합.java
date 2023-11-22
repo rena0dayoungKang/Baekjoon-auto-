c680072e1f73c8a439b23e545bd4e811b5f219e7
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-11-22 15:58:31 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -4,6 +4,7 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         
         int N = Integer.parseInt(sc.nextLine());
+        //int N = sc.nextInt(); 을 안쓰는 이유는 nextInt()는 개행을 읽을 수 없기 때문에 아래에서 nextLine() 을 읽는다면 공백이 str에 저장되게 됨
         String str = sc.nextLine();
         String[] numbers = str.split("");
         
